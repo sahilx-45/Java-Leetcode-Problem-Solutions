d9774ac8948052979e07416bc5490f914d5ecf2f
Sahil Naykodi <sahilnaykodi084@gmail.com>
2025-09-11 23:19:17 +0530

    Insertion sort algorithm

---
+++ insertion_sort.java
@@ -0,0 +1,13 @@
+public class insertion_sort {
+    public static void main(String[] args) {
+        int arr[]={4,3,6,1,8};
+
+    }
+    public static void bubble_sort(int[]arr){
+        for(int i=0;i<=arr.length-2-i;i++){
+            for(int j=1;j<i;j++){
+                
+            }
+        }
+    }
+}
