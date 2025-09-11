a3d6ca4b9fe4cdb1abf3506f6566e588b377e694
Sahil Naykodi <sahilnaykodi084@gmail.com>
2025-09-11 23:00:53 +0530

    Refactor selection sort implementation for improved readability and consistency

--- selection_sort.java
+++ selection_sort.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
 public class selection_sort {
     public static void main(String[] args) {
-        int arr[]={3,1,6,5,2};
+        int[] arr={3,1,6,5,2};
+        selection_sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void selection_sort(int[]arr){
+        for(int i=0;i<arr.length;i++){
+            int last=arr.length-1-i;
+            int maxindex=getMaxIndex(arr,0,last);
+            swap(arr,maxindex,last);
+        }
+    }
+    static int getMaxIndex(int[]arr,int start,int end){
+        int max=start;
+        for(int i=start;i<=end;i++){
+            if(arr[max]<arr[i]){
+                max=i;
+            }
+        }
+        return max;
+    }
+    public static void swap(int []arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
     }
 }
